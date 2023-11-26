5f3eb436901d9ebe28a6345999d98d6cc94da514
Anita889 <manahit889@gmail.com>
2023-11-26 17:03:51 +0400

    Hii

---
+++ lec_10/__init__.py
@@ -0,0 +1,21 @@
+from vehicle import Vehicle
+
+class Car(Vehicle):
+    def __init__(self):
+        super().__init__()
+
+class Plane(Vehicle):
+    def __init__(self):
+        super().__init__()
+
+class Boat(Vehicle):
+    def __init__(self):
+        super().__init__()
+
+class RaceCar(Car):
+    def __init__(self):
+        super().__init__()
+
+        class Vehicle:
+    def __init__(self):
+        pass
